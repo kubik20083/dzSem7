dce73458281d15632d32d2a132863fc1949f2d9d
kubik20083 <kubik20083@mail.ru>
2024-04-11 22:43:24 +0300

    task1

--- task1.c
+++ task1.c
@@ -1,9 +1,17 @@
 // Задача 1. Среднее арифметическое чисел
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    
+    int arr[5];
+    printf("enter five numbers: \n");
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+        sum += arr[i];
+    printf("average = %.3f", (float)sum / 5);
     return 0;
 }
